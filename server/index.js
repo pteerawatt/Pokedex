bb00e888513e1dc4f8c3ac970068975dbcecaf7b
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-09-30 14:06:40 -0700

    finished /species

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require('path')
-const fetch = require('node-fetch')
-const storage = require('./storage')
+const path = require('path');
+const fetch = require('node-fetch');
+const storage = require('./storage');
 
 const app = express();
 const port = 3150;
@@ -14,8 +14,8 @@ app.get('/api/pokemon', (req, res) => {
   // get data from /pokemon
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
   .then(request => {
-    if (request.ok) return request.json()
-    else res.send('ERROR')
+    if (request.ok) return request.json();
+    else res.send('ERROR: in search pokemon');
   })
   .then(data => {
     storage.id = data.id;
@@ -27,17 +27,28 @@ app.get('/api/pokemon', (req, res) => {
     // get all posibile locations for the pokemon
     fetch(data.location_area_encounters)
     .then(resLoc => {
-      if (resLoc.ok) return resLoc.json()
-      else res.send('ERROR: in search location')
+      if (resLoc.ok) return resLoc.json();
+      else res.send('ERROR: in search location');
     })
     .then(data => {
-      storage.locations = data.map((e) => e.location_area.name)
-      res.send(storage)
-      // get data from /pokemon-species
-      // fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`)
-      // .then(() => {
-
-      // })
+      storage.locations = data.map((e) => e.location_area.name);
+      
+      //get data from /pokemon-species
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`)
+      .then(resSpec => {
+        if (resSpec.ok) return resSpec.json();
+        else res.send('ERROR: in search species');
+      })
+      .then(data => {
+        storage.color = data.color.name;
+        storage.genders = data.gender_rate;
+        storage.varieties = data.varieties.map(e => {
+          const url = e.pokemon.url.split('/');
+          const urlId = url[url.length - 2]
+          if (urlId != storage.id) return urlId
+        })
+        res.send(data)
+      })
     })
   })
 })
