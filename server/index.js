3c77e8637ccf366cceb9b969c7e0d9f2fa49c2df
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-09-30 13:27:56 -0700

    stored all data from /pokemon but need to clean up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path')
 const fetch = require('node-fetch')
+const storage = require('./storage')
 
 const app = express();
 const port = 3150;
@@ -10,11 +11,28 @@ app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.get('/api/pokemon', (req, res) => {
   let pokemon = req.query.pokemon.toLowerCase();
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then((res) => {
-      return res.json()
+  .then((request) => {
+    if (request.ok) return request.json()
+    else res.send('ERROR')
+  })
+  .then((data) => {
+    storage.id = data.id;
+    storage.name = data.name;
+    storage.abilities = data.abilities.map((e) => e.ability.name);
+    storage.moves = data.moves.map((e) => e.move.name);
+    storage.types = data.types.map((e) => e.type.name);
+
+    // get all posibile locations for the pokemon
+    fetch(data.location_area_encounters)
+    .then((resLoc) => {
+      if (resLoc.ok) return resLoc.json()
+      else res.send('ERROR: in search location')
     })
-    .then((data) => res.send(data))
-    .catch((err) => console.log(err))
+    .then((data) => {
+      storage.locations = data.map((e) => e.location_area.name)
+      res.send(storage)
+    })
+  })
 })
 
 app.listen(port, () => console.log(`Pokedex on port: ${port}`))
